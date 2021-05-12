cc3f07f7c58483b9e9b72ccdd4944c667e2ea011
Amanda Hamilton <the.a.hamilton@gmail.com>
2021-05-12 14:37:26 -0400

    getting on track for a merge.

---
+++ controllers/shiftsController.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+
+// Defining methods for the ShiftsController
+module.exports = {
+  findAll: function(req, res) {
+    db.Shift
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Shift
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Shift
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Shift
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Shift
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
