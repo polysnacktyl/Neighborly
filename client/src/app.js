9c3dfc5f35421d92cc326f0df0444e3f065bf04b
Tonya Fletcher <tfletch4504@gmail.com>
2021-05-12 21:34:45 -0400

    community protected route

--- client/src/app.js
+++ client/src/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Route, Switch } from "react-router-dom";
 import NoMatch from "./views/NoMatch";
 import { NavBar, Footer } from "./components";
-import { Home, Profile, Books, Detail, Shifts, ShiftDetails } from "./views";
+import { Home, Profile, Books, Detail, Shifts, ShiftDetails, Community } from "./views";
 import ProtectedRoute from "./auth/protected-route";
 import "./app.css";
 
@@ -14,6 +14,7 @@ const App = () => {
       <Switch>
         <Route exact path="/"> <Home /></Route>
         <ProtectedRoute path="/profile" component={Profile}/>
+        <ProtectedRoute path="/community" component={Community} />
         <ProtectedRoute exact path="/books" component={Books}/>
         <ProtectedRoute exact path="/books/:id" component={Detail}/>
         <ProtectedRoute exact path="/shifts" component={Shifts}/> 
---
+++ client/src/views/Community.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Community() {
+    return (
+        <div className="text-center" style={{ color: "#00ffcc" }}>
+            <p>
+                Test Text Here
+                </p>
+            </div>
+    );
+}
+
+export default Community;
+
