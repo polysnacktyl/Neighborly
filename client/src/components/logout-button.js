be8ced9739d3314a1f39b58fd11518a9fe45142d
Tonya Fletcher <tfletch4504@gmail.com>
2021-05-14 13:48:17 -0400

    styled background

--- client/src/components/logout-button.js
+++ client/src/components/logout-button.js
@@ -5,7 +5,7 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
   return (
     <button
-      className="btn btn-danger btn-block"
+      className="btn btn-light btn-block"
       onClick={() =>
         logout({
           returnTo: window.location.origin,
