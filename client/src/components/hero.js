3ee9551796c04e7273f990cffcf0035cb2fc8b57
Tonya Fletcher <tfletch4504@gmail.com>
2021-05-13 23:01:07 -0400

    homepage style

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Hero = () => (
-  <div className="text-center hero">
-
-    <h1 className="mb-4">project 3!</h1>
-    <p className="lead">
-     welcome to our...content...probably. 
-    </p>
+  <div className="text-center hero-home">
+    <h1 className="mb-4">Neighborly</h1>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
   </div>
 );
 
--- client/src/components/home-content.js
+++ client/src/components/home-content.js
@@ -1,36 +1,36 @@
 import React from "react";
 
+
 const HomeContent = () => (
+  
   <div className="next-steps">
-    <h2 className="my-5 text-center">What Content Shall Go Here?</h2>
+    <h2 className="my-5 text-center"
+    style={{
+      color: "var(--pale)",
+      fontWeight: "bolder",
+      fontSize: "24px",
+      textShadow: "2px 2px var(--forest)"
 
-    <div className="row">
-      <div className="col-md-5 mb-4">
-        <h6 className="mb-3">
-          Lorem Firstum
-        </h6>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+    }}>
+    ...neighbors helping neighbors get through hard times &#128151;</h2>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
       </div>
-
-      <div className="col-md" />
-
-      <div className="col-md-5 mb-4">
-        <h6 className="mb-3">
-          Lorem Secondum
-        </h6>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-    </div>
+  
 
 
 
 
 
-  </div>
+  
 );
 
 export default HomeContent;
