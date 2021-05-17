ad17053494e1d94f00de7759671c483b41195da2
Amanda Hamilton <the.a.hamilton@gmail.com>
2021-05-17 16:47:52 -0400

    yay

--- client/src/index.js
+++ client/src/index.js
@@ -8,11 +8,7 @@ import "./index.css";
 
 ReactDOM.render(
   <Router>
-    <Auth0ProviderWithHistory
-    domain="divine-math-3885.us.auth0.com"
-    clientId="5E399ofqYs1Fu6vvOkCiJlMZqKI9v8Tc"
-    redirectUri={window.location.origin}
-    audience={"https://infinite-forest-58133.herokuapp.com/"}> 
+    <Auth0ProviderWithHistory>    
       <App />
     </Auth0ProviderWithHistory>
   </Router>,
