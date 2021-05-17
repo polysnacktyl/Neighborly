ddfb7f7cb1a2ee5573c35e02820c72aa79d24a5a
Matthew Ward <mattrward1030@gmail.com>
2021-05-17 10:39:08 -0400

    added user to all models and fixed requests page

--- client/src/views/requests.js
+++ client/src/views/requests.js
@@ -20,7 +20,7 @@ function Requests() {
     let userID = user.sub.split("|");
     let authID = userID[1];
     API.getRequests(authID)
-      .then(res => 
+      .then(res =>
         setRequests(res.data)
       )
       .catch(err => console.log(err));
@@ -34,7 +34,7 @@ function Requests() {
 
   function handleInputChange(event) {
     const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
+    setFormObject({ ...formObject, [name]: value })
   };
 
   function handleFormSubmit(event) {
@@ -45,6 +45,7 @@ function Requests() {
       API.saveRequests({
         title: formObject.title,
         request: formObject.request,
+        user: user.email,
         authID: authID
       })
         .then(res => loadRequests())
@@ -52,51 +53,57 @@ function Requests() {
     }
   };
 
-    return (
-      <Container fluid>
-        <Row>
-          <Col size="md-6 sm-12">
-            <form>
-              <Input
-                onChange={handleInputChange}
-                name="title"
-                placeholder="title (required)"
-              />
-              <TextArea
-                onChange={handleInputChange}
-                name="request"
-                placeholder="request (required)"
-              />
-              <FormBtn
-                disabled={!(formObject.title && formObject.request)}
-                onClick={handleFormSubmit}
-              >
-                Submit Request
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-6 sm-12">
+          <form>
+            <Input
+              onChange={handleInputChange}
+              name="title"
+              placeholder="title (required)"
+            />
+            <TextArea
+              onChange={handleInputChange}
+              name="request"
+              placeholder="request (required)"
+            />
+            <div
+              onChange={handleInputChange}
+              name="user"
+              value={user.email}
+
+            />
+            <FormBtn
+              disabled={!(formObject.title && formObject.request)}
+              onClick={handleFormSubmit}
+            >
+              Submit Request
               </FormBtn>
-            </form>
-          </Col>
-          <Col size="md-6 sm-12">
-            {Requests.length ? (
-              <List>
-                {Requests.map(requests => (
-                  <ListItem key={requests._id}>
-                    <Link to={"/requests/" + requests._id}>
-                      <strong>
-                        {requests.title}, {requests.request}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => deleteRequests(requests._id)} />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
+          </form>
+        </Col>
+        <Col size="md-6 sm-12">
+          {Requests.length ? (
+            <List>
+              {Requests.map(requests => (
+                <ListItem key={requests._id}>
+                  <Link to={"/requests/" + requests._id}>
+                    <strong>
+                      {requests.title}, {requests.request}
+                    </strong>
+                  </Link>
+                  <DeleteBtn onClick={() => deleteRequests(requests._id)} />
+                </ListItem>
+              ))}
+            </List>
+          ) : (
               <h3>No Results to Display</h3>
             )}
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 
 export default Requests;
