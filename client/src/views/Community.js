53edd985278ed104db88b4d0e90ba24ef1a93d42
Matthew Ward <mattrward1030@gmail.com>
2021-05-17 10:56:17 -0400

    fixed styling on cards

--- client/src/views/Community.js
+++ client/src/views/Community.js
@@ -19,35 +19,38 @@ function Community() {
                     <div className="card text-center" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Request</h5>
-                            <p className="card-text">Submit a new request or fulfil a neighbor's request</p>
+
                             <div className="card-footer text-center">
                                 <Link to="/community_requests">
                                     <button className="btn-view">View Requests</button>
                                 </Link>
                             </div>
+                            <p className="card-text">Submit a new request or fulfil a neighbor's request</p>
                         </div>
                     </div>
                     <div className="card text-center" style={{ width: "18rem" }}>
                         <div className="card-body">
                             <h5 className="card-title">Resource Offers</h5>
-                            <p className="card-text">Submit a new offer or fulfill an existing offer</p>
+
                             <div className="card-footer text-center">
                                 <Link to="/community_resources">
                                     <button className="btn-view">View Resource Offers</button>
                                 </Link>
                             </div>
+                            <p className="card-text">Submit a new offer or fulfill an existing offer</p>
                         </div>
                     </div>
                     <div className="card text-center" style={{ width: "18rem" }}>
                         <div className="card-body">
-                            <h5 className="card-title">Volunteer Shift Calendar</h5>
-                            <p className="card-text">Calendar of upcoming volunteer events in the community</p>
+                            <h5 className="card-title">Volunteering</h5>
                             <div className="card-footer text-center">
                                 <Link to="/community_calendar">
                                     <button className="btn-view">View Calendar</button>
                                 </Link>
                             </div>
+                            <p className="card-text">Calendar of upcoming volunteer events in the community</p>
                         </div>
+
                     </div>
                 </Row>
             </Container >
